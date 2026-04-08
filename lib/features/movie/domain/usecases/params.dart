e738fba0049013626669b171b8434488bc78135d
Virendra Kumar <vkumar@medocity.com>
2026-04-08 23:24:25 +0530

    fix: added missing file

---
+++ lib/features/movie/domain/usecases/params.dart
@@ -0,0 +1,18 @@
+class PageParams {
+  final int page;
+
+  PageParams(this.page);
+}
+
+class MovieDetailsParams {
+  final int movieId;
+
+  MovieDetailsParams(this.movieId);
+}
+
+class SearchMoviesParams {
+  final String query;
+  final int page;
+
+  SearchMoviesParams(this.query, this.page);
+}
