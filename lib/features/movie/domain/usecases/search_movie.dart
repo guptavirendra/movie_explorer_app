970ff75ec8fef25b72f5739b6645a2b7f8142809
Virendra Kumar <vkumar@medocity.com>
2026-04-08 23:05:03 +0530

    feat(domain): implement SearchMovies use case

---
+++ lib/features/movie/domain/usecases/search_movie.dart
@@ -0,0 +1,21 @@
+import 'package:movie_explorer_app/core/util/usecase.dart';
+import 'package:movie_explorer_app/features/movie/domain/entities/movies.dart';
+import 'package:movie_explorer_app/features/movie/domain/repositories/movie_repository.dart';
+
+class SearchMovies implements UseCase<List<Movie>, SearchParams> {
+  final MovieRepository repository;
+
+  SearchMovies(this.repository);
+
+  @override
+  Future<List<Movie>> call(SearchParams params) async {
+    return await repository.searchMovies(params.query, params.page);
+  }
+}
+
+class SearchParams {
+  final String query;
+  final int page;
+
+  SearchParams({required this.query, required this.page});
+}
