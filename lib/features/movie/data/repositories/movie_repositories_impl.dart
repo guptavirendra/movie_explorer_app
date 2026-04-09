4426830ff33ea7c6f740f1c9fe4f16c80def2566
Virendra Kumar <vkumar@medocity.com>
2026-04-09 22:33:42 +0530

    feat(data): implement MovieRepositoryImpl (remote + local integration)

---
+++ lib/features/movie/data/repositories/movie_repositories_impl.dart
@@ -0,0 +1,51 @@
+import 'package:movie_explorer_app/features/movie/data/datasources/movie_local_datasource.dart';
+import 'package:movie_explorer_app/features/movie/data/datasources/movie_remote_data_source.dart';
+import 'package:movie_explorer_app/features/movie/data/models/movie_model.dart';
+import 'package:movie_explorer_app/features/movie/domain/entities/movies.dart';
+import 'package:movie_explorer_app/features/movie/domain/repositories/movie_repository.dart';
+
+class MovieRepositoriesImpl implements MovieRepository {
+  final MovieRemoteDataSource remoteDataSource;
+  final MovieLocalDatasource localDataSource;
+  MovieRepositoriesImpl(this.remoteDataSource, this.localDataSource);
+  @override
+  Future<List<Movie>> getPopularMovies(int page) async {
+    final movies = await remoteDataSource.getPopularMovies(page);
+    return movies.map((movie) => movie.toEntity()).toList();
+  }
+
+  @override
+  Future<Movie> getMovieDetails(int movieId) async {
+    final result = await remoteDataSource.getMovieDetails(movieId);
+    return result.toEntity();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    final model = MovieModel(
+      id: movie.id,
+      title: movie.title,
+      overview: movie.overview,
+      posterPath: movie.posterPath,
+      backdropPath: movie.posterPath,
+      //rating: movie.rating,
+      voteAverage: 1.0, // Placeholder, replace with actual rating if available
+      releaseDate: movie.releaseDate,
+      voteCount: 1,
+    );
+
+    await localDataSource.toggleFavorite(model);
+  }
+
+  @override
+  Future<List<Movie>> getFavoriteMovies() async {
+    final favoriteMovies = await localDataSource.getFavorites();
+    return favoriteMovies.map((movie) => movie.toEntity()).toList();
+  }
+
+  @override
+  Future<List<Movie>> searchMovies(String query, int page) async {
+    final result = await remoteDataSource.searchMovies(query);
+    return result.map((e) => e.toEntity()).toList();
+  }
+}
