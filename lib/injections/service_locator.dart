5f13486a0b5e42b7f4c178ae4bac85581619e9bb
Virendra Kumar <vkumar@medocity.com>
2026-04-09 22:42:23 +0530

    feat(data): register data sources and repository in GetIt

--- lib/injections/service_locator.dart
+++ lib/injections/service_locator.dart
@@ -1,9 +1,12 @@
-
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_explorer_app/core/network/dio_client.dart';
 import 'package:movie_explorer_app/core/network/network_info.dart';
+import 'package:movie_explorer_app/features/movie/data/datasources/movie_local_datasource.dart';
+import 'package:movie_explorer_app/features/movie/data/datasources/movie_remote_data_source.dart';
+import 'package:movie_explorer_app/features/movie/data/repositories/movie_repositories_impl.dart';
 
 final singleton = GetIt.instance;
 
@@ -13,10 +16,17 @@ Future<void> init() async {
   singleton.registerLazySingleton(() => Dio());
   singleton.registerLazySingleton(() => DioClient(singleton()));
 
-// Connectivity
+  // Connectivity
   singleton.registerLazySingleton(() => Connectivity());
   singleton.registerLazySingleton(() => NetworkInfo(singleton()));
-  
-    
-  
+
+  // Movie Data Sources and Repository
+  singleton.registerLazySingleton(() => MovieRemoteDataSourceImpl(singleton()));
+  singleton.registerLazySingleton(() => MovieLocalDatasourceImpl(singleton()));
+  singleton.registerLazySingleton(
+    () => MovieRepositoriesImpl(singleton(), singleton()),
+  );
+  // Hive Box
+  final box = await Hive.openBox('favorites');
+  singleton.registerLazySingleton(() => box);
 }
