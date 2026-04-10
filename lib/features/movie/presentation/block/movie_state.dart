3ed8551b6c0589375a4b9af7871391637922ce95
Virendra Kumar <vkumar@medocity.com>
2026-04-10 22:09:08 +0530

    feat(presentation): implement MovieState (loading, loaded, error, pagination)

---
+++ lib/features/movie/presentation/block/movie_state.dart
@@ -0,0 +1,26 @@
+abstract class MovieState {
+  get props => [];
+}
+
+class MovieInitial extends MovieState {}
+
+class MovieLoading extends MovieState {}
+
+class MovieLoaded extends MovieState {
+  final List movies;
+  final bool hasReachedMax;
+
+  MovieLoaded({required this.movies, required this.hasReachedMax});
+
+  @override
+  get props => [movies, hasReachedMax];
+}
+
+class MovieError extends MovieState {
+  final String message;
+
+  MovieError({required this.message});
+
+  @override
+  get props => [message];
+}
