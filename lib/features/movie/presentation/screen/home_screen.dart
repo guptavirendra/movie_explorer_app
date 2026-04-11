63ffd61de4e10195fdee27af1870897f35f902d1
Virendra Kumar <vkumar@medocity.com>
2026-04-11 15:45:52 +0530

    feat(presentation): implement HomeScreen with Bloc, pagination, and refresh

---
+++ lib/features/movie/presentation/screen/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_explorer_app/features/movie/presentation/block/movie_bloc.dart';
+import 'package:movie_explorer_app/features/movie/presentation/block/movie_event.dart';
+import 'package:movie_explorer_app/features/movie/presentation/block/movie_state.dart';
+import 'package:movie_explorer_app/features/movie/presentation/widget/movie_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    context.read<MovieBloc>().add(FetchPopularMovies());
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent - 200) {
+        context.read<MovieBloc>().add(LoadMoreMovies());
+      }
+    });
+  }
+
+  Future<void> _onRefresh() async {
+    context.read<MovieBloc>().add(RefreshMovies());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Movies")),
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: BlocBuilder<MovieBloc, MovieState>(
+          builder: (context, state) {
+            if (state is MovieLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (state is MovieError) {
+              return Center(child: Text(state.message));
+            }
+
+            if (state is MovieLoaded) {
+              return GridView.builder(
+                controller: _scrollController,
+                itemCount: state.movies.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                ),
+                itemBuilder: (_, index) {
+                  final movie = state.movies[index];
+
+                  return MovieCard(movie: movie, onTap: () {});
+                },
+              );
+            }
+
+            return const SizedBox();
+          },
+        ),
+      ),
+    );
+  }
+}
