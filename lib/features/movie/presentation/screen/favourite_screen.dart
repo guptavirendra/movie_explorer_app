f2e499364b25ce8bd5bbed84bc0df80eea5dfe5b
Virendra Kumar <vkumar@medocity.com>
2026-04-11 23:50:50 +0530

    feat(favorites): implement FavoritesScreen UI with remove option

---
+++ lib/features/movie/presentation/screen/favourite_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_explorer_app/features/movie/presentation/cubit/favourites_cubit.dart';
+import 'package:movie_explorer_app/features/movie/presentation/cubit/favourites_state.dart';
+import 'package:movie_explorer_app/injections/service_locator.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => singleton<FavouritesCubit>()..loadFavorites(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Favourites')),
+        body: BlocBuilder<FavouritesCubit, FavouritesState>(
+          builder: (context, state) {
+            if (state is FavouritesLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (state is FavouritesError) {
+              return Center(child: Text(state.message));
+            }
+
+            if (state is FavouritesEmpty) {
+              return const Center(child: Text('No favourites yet!'));
+            }
+
+            if (state is FavouritesLoaded) {
+              return ListView.builder(
+                itemCount: state.movies.length,
+                itemBuilder: (_, index) {
+                  final movie = state.movies[index];
+
+                  return ListTile(
+                    leading: Image.network(
+                      "https://image.tmdb.org/t/p/w200${movie.posterPath}",
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(movie.title),
+                    subtitle: Text("⭐ ${movie.rating}"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        context.read<FavouritesCubit>().removeFavorite(movie);
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const SizedBox.shrink();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesCubit {}
