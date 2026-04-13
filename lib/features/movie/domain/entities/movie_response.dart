dee3ca17df0a37deb0eec60f989cfb36165d8874
Virendra Kumar <vkumar@medocity.com>
2026-04-13 19:55:47 +0530

    refactor(domain): make MovieResponse equatable value object

--- lib/features/movie/domain/entities/movie_response.dart
+++ lib/features/movie/domain/entities/movie_response.dart
@@ -1,8 +1,13 @@
+import 'package:equatable/equatable.dart';
+
 import 'movie.dart';
 
-class MovieResponse {
+class MovieResponse extends Equatable {
   final List<Movie> movies;
   final int totalPages;
 
-  MovieResponse({required this.movies, required this.totalPages});
+  const MovieResponse({required this.movies, required this.totalPages});
+
+  @override
+  List<Object?> get props => [movies, totalPages];
 }
